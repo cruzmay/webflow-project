7fe1179ac1331cae6a5fcb24fb3a0de6ec2c8709
Pedro Cruz <pcruzmay@yahoo.com>
2021-10-18 17:44:58 -0500

    add button

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,29 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useForm } from '../../hooks/useForms'
+
 
 export const Login = () => {
+    const [ {email, password }, handleInputChange ] = useForm({
+        email: 'pcruzmay@yahoo.com',
+        password: '123456'
+    })
     return (
-        <div className="login">
-            <h1>Login</h1>
-            <input 
-            type="text" 
-            placeholder="email"
-            name="email"
-            autoComplete="off"
-            />
-            <input 
-            type="password" 
-            placeholder="password"
-            name="password"
-            autoComplete="off"
-            />
-            <button
-            type="submit"
-            >
-                Login
-            </button>
-            <Link to="#">Forgot Password?</Link>
-            <Link to="/sing-up">don't have an account? Sing up...</Link>
+        <div className="ww-comp-button">
+            <h3 className="ww-type-paragraph color-secondary typebold primarybutton">Paragraph</h3>
         </div>
     )
 }
