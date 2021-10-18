12b1c5904823f868b6c88e762500792571fff143
Pedro Cruz <pcruzmay@yahoo.com>
2021-10-18 18:34:35 -0500

    fix

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { useForm } from '../../hooks/useForms'
 
 
 export const Login = () => {
-
+    
     useEffect(() => {
       Outseta.on('accessToken.set', function (user) {
         console.log('accessToken.set', user);
         });
-    }, [])
+    }, []
 
     return (
         <div className="ww-comp-button">
