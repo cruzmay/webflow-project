38c070d153c049f2031e817a1a724ac4cb000caf
Pedro Cruz <pcruzmay@yahoo.com>
2021-10-18 18:31:47 -0500

    outseta function test

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -4,10 +4,13 @@ import { useForm } from '../../hooks/useForms'
 
 
 export const Login = () => {
-    const [ {email, password }, handleInputChange ] = useForm({
-        email: 'pcruzmay@yahoo.com',
-        password: '123456'
-    })
+
+    useEffect(() => {
+      Outseta.on('accessToken.set', function (user) {
+        console.log('accessToken.set', user);
+        });
+    }, [])
+
     return (
         <div className="ww-comp-button">
             <h3 className="ww-type-paragraph color-secondary typebold primarybutton">Paragraph</h3>
