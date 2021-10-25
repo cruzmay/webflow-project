847281f7a5258310363b9b5847ab3fb0f6ca82e0
Pedro Cruz <pcruzmay@yahoo.com>
2021-10-25 00:01:36 -0500

    firebase auth

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,6 +1,7 @@
-import { addDoc, collection } from '@firebase/firestore'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from '@firebase/auth'
+import { addDoc, collection, getDocs } from '@firebase/firestore'
 import React, { useEffect, useState } from 'react'
-import { db } from '../../firebase/config'
+import { auth, db } from '../../firebase/config'
 
 export const Login = () => {
 
@@ -19,13 +20,25 @@ useEffect(() => {
 
 console.log(user)
 
-// try {
-//     const docRef = await addDoc( collection(db, 'users'), user)
-//     console.log("Document written with ID: ", docRef.id);
-// } catch (e) {
-//     console.error("error adding dcument: ", e)
-// }
 
+// const addUser = async () => {
+//     try {
+//         const docRef = await addDoc( collection(db, 'users'), user)
+//         console.log("Document written with ID: ", docRef.id);
+//     } catch (e) {
+//         console.error("error adding document: ", e)
+//     }
+// }
+// addUser()
+
+const getUsers = async () => {
+    const querySnapshot = await getDocs(collection(db, "users"));
+    querySnapshot.forEach((doc) => {
+     const data = doc.data()
+     console.log(data)
+});
+}
+getUsers()
     return (
         <div className="ww-comp-button">
             <h3 className="ww-type-paragraph color-secondary typebold primarybutton">{user.FullName}</h3>
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,5 +20,6 @@ const app = initializeApp(firebaseConfig);
 console.log(app);
 const db = getFirestore();
 console.log(db);
+const auth = getAuth()
 
-export { db, app };
+export { db, app, auth };
