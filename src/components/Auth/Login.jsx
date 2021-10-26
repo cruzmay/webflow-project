6c200ab8b88e46ba38cbe1591e2d391e5c10b951
Pedro Cruz <pcruzmay@gmail.com>
2021-10-25 23:06:21 -0500

    integration bd

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,21 +1,16 @@
-import { addDoc, collection, doc, getDoc, getDocs, query, setDoc, where } from '@firebase/firestore'
+import { collection, doc, getDoc, setDoc } from '@firebase/firestore'
 import React, { useEffect, useState } from 'react'
 import { db } from '../../firebase/config'
 
 export const Login = () => {
 
-const [user, setuser] = useState({
-        loading: true,
-        loginId:'',
-        name: '',
-        lastname: '',
-        email: '',})
+const [user, setuser] = useState({})
+const [dbUser, setdbUser] = useState({})
 
 useEffect(() => {
     new Promise ((resolve) => {
        resolve(Outseta.getUser())
     }).then( data => ({
-        loading: false,
         loginId:  data.Uid,
         name: data.FirstName,
         lastname: data.LastName,
@@ -25,50 +20,30 @@ useEffect(() => {
 }, [])
 
 
-// const addUser = async () => {
-//     try {
-//         const docRef = await addDoc( collection(db, 'users'), user)
-//         console.log("Document written with ID: ", docRef.id);
-//     } catch (e) {
-//         console.error("error adding document: ", e)
-//     }
-// }
-// addUser()
-
 useEffect(() => {
-
-    
-
 
     const createUser = async () => {
 
-        const userRef = collection(db, "users");
-
-        await setDoc(doc(userRef,`${user.loginId}`), user);
-
         const docRef = doc(db, "users", `${user.loginId}`);
         const docSnap = await getDoc(docRef);
-           if(docSnap.exist()) {
-               console.log("exist")
-           } else {
-               console.log('dont exist')
-           }
+
+        if (docSnap.exists()) {
+            const dbUser = docSnap.data()
+            setdbUser(dbUser)
+        } else {
+            const userRef = collection(db, "users");
+            await setDoc(doc(userRef,`${user.loginId}`), user);
+
+            const docRef = doc(db, "users", `${user.loginId}`);
+            const docSnap = await getDoc(docRef);
+            const dbUser = docSnap.data()
+            setdbUser(dbUser)
+        }
     }
     createUser()
 }, [user, setuser])
 
-
-useEffect(() => {
-const getUsers = async () => {
-    const userDashboard = collection(db, "users");
-            const userQuery =  query(userDashboard, where("loginId", "==", `${user.loginId}` ));
-                    const querySnapshot = await getDocs(userQuery)
-                    querySnapshot.forEach( (doc) => {
-                        console.log(doc.id , ' => ', doc.data())
-                    })
-}
-getUsers()
-}, [user, setuser])
+console.log('state', dbUser)
 
     return (
         <>
